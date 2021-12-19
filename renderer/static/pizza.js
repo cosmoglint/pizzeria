3429d8bdefe937e784f2bcbd08ad6b55844f9790
cosmoglint <ramprakashsmail@gmail.com>
2021-12-19 20:24:25 +0530

    ADD:  loading of json file

--- renderer/static/pizza.js
+++ renderer/static/pizza.js
@@ -1,32 +1,38 @@
-contents = require('./contnet/mappings.json')
-
+// contents = require('./contnet/mappings.json')
+contents = fetch('./content/mappings.json').then(response => response.json());
 
 function Pizza(radius, base, customization){
   this.radius = radius;
-  this.base = base;
   this.color = color;
   this.customization = customization;
+  this.image_preload = false;
 }
 
 Pizza.prototype = {
   show: function(){
-    circle(wcx,wcy,this.radius);
+    if (this.image_preloaded) {
+      circle(wcx,wcy,this.radius);
+    }
   },
   gather_ingredients: function(key){
-    custom_value = this.customization[key]
-    data = contents[custom_value]
+    let custom_value = this.customization[key];
+    let data = contents[custom_value];
     return data
   },
   load_assets: function(){
-    base = gather_ingredients(base)
-    cheese = gather_ingredients(cheese)
-    topping = gather_ingredients(topping)
-    sauce = gather_ingredients(sauce)
-    
+    let base = gather_ingredients(base);
+    let cheese = gather_ingredients(cheese);
+    let topping = gather_ingredients(topping);
+    let sauce = gather_ingredients(sauce);
+    loadImage(base, img => {
+      this.base = img;
+      loadImage(cheese, img => {
+        this.cheese = img;
+        loadImage(topping, img => {
+          this.topping = img;
+          parent.image_loaded = true;
+        });
+      });
+    });
   },
-  load_image: function(location){
-   loadImage(location, img => {
-    image(img, 0, 0);
-  });
-  }
 }
