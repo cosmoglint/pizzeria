592a13e93cde3ce6d8247cc70a76d582ffe9dbae
cosmoglint <ramprakashsmail@gmail.com>
2021-12-19 08:21:06 +0530

    ADD:  backend api calls

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const { expressPort } = require('./config');
+const app = express();
+
+const { generate_image } = require('./main')
+
+app.get('/', (req,res) => res.send("hello world bro"));
+
+app.get('/image', async (req,res) => {
+
+  var image_buffer = await generate_image();
+  res.setHeader('content-type', 'image/png');
+  res.write(image_buffer, 'binary');
+  res.end(null, 'binary');
+})
+
+app.listen(expressPort, () => console.log(`listening on port ${expressPort} bruh`))
+
+
+function render_pizza(conf){
+  // render the pizza accordingly
+}
+
